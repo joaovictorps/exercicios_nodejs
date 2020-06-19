bc317f9dd658a810468ddc69b6c469e58134a488
João Victor <victorps.joao@gmail.com>
2020-06-19 20:13:44 -0300

    Exercício 3 resolvido

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,15 @@
 // ## Importe a biblioteca ##
-
+let rs = require('readline-sync')
 
 
 // ## Faça o código ##
+console.log("===================================")
+console.log("Calcule a area de um QUADRADO")
+console.log("===================================")
+let numero = rs.questionFloat('Qual o tamanho do lado do quadrado ? \n')
+
+let area = numero * numero
+
+console.log("===================================")
+console.log("A área do quadrado é "+area +" .")
+console.log("===================================")
