299ff510158fc8d7dd0b7f2d0b229bdaedae129f
João Victor <victorps.joao@gmail.com>
2020-06-19 22:18:48 -0300

    Exercicios resolvidos

--- atividade_5/index.js
+++ atividade_5/index.js
@@ -1,5 +1,9 @@
 // ## Importe a biblioteca ##
-
+let rs = require('readline-sync')
 
 
 // ## Faça o código ##
+
+let caracteres = rs.question('Digite alguma coisa.\n')
+
+console.log('Você digitou ' + caracteres.length +' caracteres.')
--- atividade_6/index.js
+++ atividade_6/index.js
@@ -1,5 +1,11 @@
 // ## Importe a biblioteca ##
 
-
+let rs = require('readline-sync')
 
 // ## Faça o código ##
+let nome = rs.question('Qual o seu nome ? \n')
+
+let caracteres = rs.question('Digite alguma coisa.\n')
+
+console.log('Você digitou ' + caracteres.length +' caracteres.')
+
