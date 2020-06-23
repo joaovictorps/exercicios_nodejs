63af62babc2f17aa43e65060b53fa2e228974071
João Victor <victorps.joao@gmail.com>
2020-06-22 22:06:08 -0300

    mudei o algoritmo de if/else para switch case e aprimorei a resposta do usuario

--- atividade_4/index.js
+++ atividade_4/index.js
@@ -2,82 +2,52 @@
 let rs = require('readline-sync')
 
 // ## Faça o código ##
-let mes = rs.questionFloat('Qual o mes que voce nasceu ? \n')
+let mes = rs.questionInt('Qual o mes que voce nasceu ? \n')
 
-let dia = rs.questionFloat('Qual o dia que voce nasceu ? \n')
+let dia = rs.questionInt('Qual o dia que voce nasceu ? \n')
 
-if (mes == 1 ){
-    if (dia >= 20){
-        console.log('Aquário')
-    }   else {
-        console.log('Capricórnio')
-    }
-} else if(mes == 2) {
-    if (dia <=18) {
-        console.log("Aquário")
-    }   else {
-        console.log('Peixes')
-    }
-} else if(mes == 3) {
-    if(dia <= 20) {
-        console.log('Peixes')
-    }   else {
-        console.log('Áries')
-    }
-} else if(mes==4) {
-    if (dia==19) {
-        console.log('Áries')
-    }   else {
-        console.log('Touro')
-    }
-} else if(mes==5) {
-    if (dia <= 20) {
-        console.log('Touro')
-    }   else {
-        console.log('Gêmeos')
-    }
-} else if(mes==6) {
-    if (dia<=21) {
-        console.log('Gêmeos')
-    }   else {
-        console.log('Câncer')
-    }
-} else if(mes==7) {
-    if (dia<=22) {
-        console.log('Câncer')
-    }   else{
-        console.log('Leão')
-    }
-} else if (mes==8) {
-    if (dia<=22) {
-        console.log('Leão')
-    }   else {
-        console.log('Virgem')
-    }
-} else if (mes==9) {
-    if (dia<=22) {
-        console.log('Virgem')
-    }   else {
-        console.log('Libra')
-    }
-} else if (mes==10) {
-    if (dia<=22) {
-        console.log('Libra')
-    }   else {
-        console.log('Escorpião')
-    }
-} else if (mes==11) {
-    if (dia<= 21) {
-        console.log('Escorpião')
-    }   else {
-        console.log('Sagitário')
-    }
-} else if (mes==12) {
-    if (dia <= 21) {
-        console.log('Sagitário')
-    } else {
-        console.log('Capricórnio')
-    }
+if ( (mes >12 || mes <= 0) || (dia >31 || dia <=0)){
+    console.log("Mês ou dia inválido")
 } else {
-    console.log('Aquário')
+    switch (mes) {
+        case 1:
+            dia >= 20 ? console.log("Aquário") : console.log("Capricórnio")
+            break
+        case 2 :
+            dia <= 18 ? console.log("Aquário") : console.log("Peixes")
+            break
+        case 3 :
+            dia <=20 ? console.log("Peixes") : console.log("Áries")
+            break
+        case 4 :
+            dia <=19 ? console.log("Áries") : console.log("Touro")
+            break
+        case 5 :
+            dia <= 20 ? console.log("Touro") : console.log("Gêmeos")
+            break
+        case 6 :
+            dia <=21 ? console.log("Gêmeos") : console.log("Câncer")
+            break
+        case 7 :
+            dia <=22 ? console.log("Câncer") : console.log("Leão")
+            break
+        case 8 :
+            dia <= 22 ? console.log("Leão") : console.log("Virgem")
+            break
+        case 9 :
+            dia <= 22 ? console.log("Virgem") : console.log("Libra")
+            break
+        case 10 :
+            dia <= 22 ? console.log("Libra") : console.log("Escorpião")
+            break
+        case 11 :
+            dia <= 21 ? console.log("Escorpião") : console.log ("Sagitário")
+            break
+        case 12 :
+            dia <= 21 ? console.log("Sagitário") : console.log("Capricórnio")
+            break
+        default:
+            console.log("Mês ou dia inválido.")
+            break
+    }
 }
